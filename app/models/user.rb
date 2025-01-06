918ce07db45ae633a3268b20ebd03ad84869150a
derrick <haylederrick14@gmail.com>
2025-01-06 12:03:14 +0300

    Update user  validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
     has_many :posts, dependent: :destroy
     has_many :comments, dependent: :destroy
     has_many :likes, dependent: :destroy
+    validates :name, presence: true
+    validates :postsCounter, presence: true, numericality: { only_integer: true , greater_than_or_equal_to: 0 }
     def threeRecentPosts
         posts.order(created_at: :desc).limit(3)
     end
