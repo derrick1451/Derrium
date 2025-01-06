cf3827cddb9f27a656bfc7e4d114b37a55fe7ddf
derrick <haylederrick14@gmail.com>
2025-01-06 12:03:03 +0300

    Update posts validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :commentsCounter, presence: true, numericality: { only_integer: true , greater_than_or_equal_to: 0 }
+  validates :likesCounter, presence: true, numericality: { only_integer: true , greater_than_or_equal_to: 0 }
   def postsCounter
     user.increment!(:postsCounter)
   end
