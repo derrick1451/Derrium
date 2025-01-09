e0a046e7440e57df4ed1f74d3e03c9bcac41a7c1
derrick <haylederrick14@gmail.com>
2025-01-09 23:33:10 +0300

    modify posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
     def index
+       @user = User.find(params[:id])
+       @posts = @user.posts
     end
     def show
+        @post = Post.find(params[:id])
     end
 end
