6e9545c60466997f2e3ef16d44fab644f4e1ab9a
derrick <haylederrick14@gmail.com>
2025-01-10 13:29:20 +0300

    Modify controller files

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,12 @@
 class PostsController < ApplicationController
-    def index
-       @user = User.find(params[:user_id])
-       @posts = @user.posts.includes(:comments, :user)
-    end
-    def show
-        @user_post = User.find(params[:user_id])
-        @posts = @user_post.posts.includes(:comments)
-        @post = @posts.find(params[:id])
-    end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.includes(:comments, :user)
+  end
+
+  def show
+    @user_post = User.find(params[:user_id])
+    @posts = @user_post.posts.includes(:comments)
+    @post = @posts.find(params[:id])
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 class UsersController < ApplicationController
-    def index
-        @users = User.all
-    end
-    def show
-        @user = User.find(params[:id])
-        if @user.nil?
-            flash[:alert] = 'User not found'
-            redirect_to users_path
-        end
-    end
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    return unless @user.nil?
+
+    flash[:alert] = 'User not found'
+    redirect_to users_path
+  end
 end
