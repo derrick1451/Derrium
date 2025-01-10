cd4bcc5db862f816435fca78e3db4d38e0b2c2d0
derrick <haylederrick14@gmail.com>
2025-01-10 13:29:44 +0300

    Modify models file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-    has_many :posts, dependent: :destroy
-    has_many :comments, dependent: :destroy
-    has_many :likes, dependent: :destroy
-    validates :name, presence: true
-    validates :postsCounter, presence: true, numericality: { only_integer: true , greater_than_or_equal_to: 0 }
-    def threeRecentPosts
-        posts.order(created_at: :desc).limit(3)
-    end
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  validates :name, presence: true
+  validates :postsCounter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  def three_recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
 end
