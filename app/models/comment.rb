55ce195c2954765bb47d370e116cbb77806790c1
derrick <haylederrick14@gmail.com>
2025-01-18 18:50:56 +0300

    Update posts/comments model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+  after_save :comments_counter
   def comments_counter
     post.increment!(:commentsCounter)
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,7 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 250 }
   validates :commentsCounter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likesCounter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  after_save :posts_counter
   def posts_counter
     user.increment!(:postsCounter)
   end
